44cf1b049d15a81dcd562b4a1a191b55687336a1
Cody Schwengler01~ <cschwengler@outlook.com>
2023-11-26 17:15:08 -0800

    updated id code

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,9 +1,9 @@
-const thought = require('../models/thought');
+const Thought = require('../models/thought');
 
 module.exports = {
     async getAllThoughts(req, res) {
         try {
-            const thoughts = await thought.find();
+            const thoughts = await Thought.find();
             res.json(thoughts);
         } catch (err) {
             res.status(500).json(err);
@@ -11,13 +11,13 @@ module.exports = {
     },
     async getSingleThought(req, res) {
         try {
-            const thought = await thought.findOne({ _id: req.params.thoughtId });
+            const singleThought = await Thought.findOne({ _id: req.params.thoughtId });
 
-            if (!thought) {
+            if (!singleThought) {
                 return res.status(404).json({ message: 'No thought with that ID' });
             }
 
-            res.json(thought);
+            res.json(singleThought);
         } catch (err) {
             res.status(500).json(err);
         }
@@ -25,30 +25,32 @@ module.exports = {
     // creates a new thought
     async createThought(req, res) {
         try {
-            const dbThoughtData = await thought.create(req.body);
-            res.json(dbThoughtData);
+            const createThoughtData = await Thought.create(req.body);
+            res.json(createThoughtData);
         } catch (err) {
-            res.status(500).json(err)
+            res.status(500).json(err);
         }
     },
     // updates a thought
     async updateThought(req, res) {
         try {
-            const dbThoughtData = await thought.findOneAndUpdate( thought._id, req.body, { new: true });
-            res.json(dbThoughtData);
+            const updateThoughtData = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId }, // Query for the specific thought by its ID
+                req.body, // Updated data
+                { new: true } // Return the updated thought
+            );
+            res.json(updateThoughtData);
         } catch (err) {
-            res.status(500).json(err)
-
+            res.status(500).json(err);
         }
     },
-    //deletes a thought
+    // deletes a thought
     async deleteThought(req, res) {
         try {
-            const dbThoughtData = await thought.findOneAndDelete( thought._id );
-            res.json(dbThoughtData);
+            const deleteThoughtData = await Thought.findOneAndDelete({ _id: req.params.thoughtId }); // Query for the specific thought by its ID
+            res.json(deleteThoughtData);
         } catch (err) {
-            res.status(500).json(err)
-
+            res.status(500).json(err);
         }
     }
 };
