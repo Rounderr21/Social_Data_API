88247a9e94feb96b479b821db8a1d59410a75c1b
Cody Schwengler01~ <cschwengler@outlook.com>
2023-11-27 19:01:11 -0800

    created a user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,51 @@
-//needs to have a username that contains a string, unique, required, and trimmed
-//email that contains a string, unique, required, must match a valid email address (look into Mongoose's matching validation), and trimmed
-//thoughts that is an array that's ids are referenced to the Thought model
-//friends that is an array that's ids are referenced to the User model (self-reference)
+const { Schema, model, Types } = require('mongoose');
 
-//shema settings create a virtual called friendCount that retrieves the length of the user's friends array field on query
+// friends schema located in the User model
+const friendsSchema = new Schema({
+    friendName: {
+        type: String,
+        required: true
+    }
+});
+
+// User Schema model
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        posts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Post'
+            }
+        ]
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/.+@.+\..+/],
+        trim: true
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [friendsSchema] // Using the friendsSchema as a subdocument within userSchema
+},
+{
+    toJSON: {
+        getters: true
+    }
+});
+
+// Virtual that retrieves the length of the user's friends array field on query
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
